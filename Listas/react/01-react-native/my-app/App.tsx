9684b0aa7a9cbac286ef99b9ed9f59fa21115fea
Mateus de carvalho Freitas <164429561+Mateus-CarvalhoFreitas@users.noreply.github.com>
2026-09-25 22:52:23 +0000

    Atualiza projeto React Native

--- Listas/react/01-react-native/my-app/App.tsx
+++ Listas/react/01-react-native/my-app/App.tsx
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>Só tem pitel!</Text>
+      <Text style={styles.text}>Olá, Meu nome é Paulo!</Text>
       <StatusBar style="auto" />
     </View>
   );
